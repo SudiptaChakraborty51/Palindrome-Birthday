df723d87c25ffdaca8dbec4f528dbaa971021d70
Sudipta <hisudipta26@gmail.com>
2022-02-16 11:43:03 +0530

    reverse a string in one line

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function reverseStr(str){
-    var listOfChars = str.split('');
-    var reverseListofChars = listOfChars.reverse();
-    var reversedStr = reverseListofChars.join('');
-    return reversedStr;
+    // var listOfChars = str.split('');
+    // var reverseListofChars = listOfChars.reverse();
+    // var reversedStr = reverseListofChars.join('');
+    // return reversedStr;
+    return str.split('').reverse().join('');
 }
 console.log(reverseStr('sudipta'));
