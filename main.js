868bf197e0bed1ff96fa2ac7471284365911c1da
Sudipta <hisudipta26@gmail.com>
2022-02-16 11:59:02 +0530

    convert number date to string

--- main.js
+++ main.js
@@ -13,3 +13,28 @@ function isPalindrome(str){
 }
 console.log(isPalindrome('madam'));
 
+function convertDateToString(date){
+    var dateStr = {
+        day: '',
+        month: '',
+        year: ''
+    }
+    if(date.day < 10){
+        dateStr.day = '0' + date.day; // convert dateStr.day to string internally
+    } else{
+        dateStr.day = date.day.toString();
+    }
+    if(date.month < 10){
+        dateStr.month = '0' + date.month;
+    } else{
+        dateStr.month = date.month.toString();
+    }
+    dateStr.year = date.year.toString();
+    return dateStr;
+}
+var date={
+    day: 14,
+    month: 9,
+    year: 2020
+}
+console.log(convertDateToString(date));
